d5f8f0344121518f1f53e5e2b26753e73a21e84c
Puspita Mistri <puspitamistri3@gmail.com>
2023-10-04 23:35:20 +0530

    EmployeeAddressMapping

---
+++ EmployeeAddress/src/main/java/com/geekster/EmployeeAddress/Model/Address.java
@@ -0,0 +1,27 @@
+package com.geekster.EmployeeAddress.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private String city;
+    private String state;
+
+    //  @Column(name = "zipcode")
+    private String zipCode;
+}
---
+++ EmployeeAddress/src/main/java/com/geekster/EmployeeAddress/Model/Employee.java
@@ -0,0 +1,28 @@
+package com.geekster.EmployeeAddress.Model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // @Column(name = "first_name")
+    private String firstName;
+
+    // @Column(name = "last_name")
+    private String lastName;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+}
